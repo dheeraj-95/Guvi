1e3535398f43dce05668217e2d1c9683e56b63ac
dheeraj <gumudavellidheeraj@gmail.com>
2020-10-11 00:36:55 +0530

    Call Back Hell Task

--- Oct - 10 - 2020/Call back Hell-Task/script.js
+++ Oct - 10 - 2020/Call back Hell-Task/script.js
@@ -1,14 +1,91 @@
 const create_class = function(elementName, className){
-    let element = document.getElementById(elementName);
+    let element = document.createElement(elementName);
     if(className){
         element.classList.add(className);
     }
     return element;
 }
 
-let h4Tag = create_class("h4");
+let bodyTag = document.querySelector("body");
+bodyTag.classList.add("my-font");
 
-h4Tag.innerText = '';
+let h1Tag = create_class("h1","center");
+
+h1Tag.innerText = '';
+document.body.append(h1Tag);
 
 
-let callbackHell = function()
+let update_counter = function(elementName,counter){
+    elementName.innerText = counter;
+    
+}
+
+let callbackHell = function(element) {
+    let i = 10;
+    
+
+    setTimeout(function(){
+        
+        update_counter(h1Tag,i);
+        setTimeout(function(){
+            i--;
+            update_counter(h1Tag,i);
+
+            setTimeout(function(){
+                i--;
+                update_counter(h1Tag,i)
+
+                setTimeout(function(){
+                    i--;
+                    update_counter(h1Tag,i);
+
+                    setTimeout(function(){
+                        i--;
+                        update_counter(h1Tag,i);
+
+                        setTimeout(function(){
+                            i--;
+                            update_counter(h1Tag, i);
+
+                            setTimeout(function(){
+                                i--;
+                                update_counter(h1Tag,i);
+
+                                setTimeout(function(){
+                                    i--;
+                                    update_counter(h1Tag,i);
+
+                                    setTimeout(function(){
+                                        i--;
+                                        update_counter(h1Tag, i);
+
+                                        setTimeout(function(){
+                                            i--;
+                                            update_counter(h1Tag, i);
+
+                                            setTimeout(function(){
+                                                update_counter(h1Tag, "Happy Independence Day");
+                                            },1000);
+
+                                        },1000);
+
+                                    },1000);
+
+                                },1000);
+
+                            },1000);
+
+                        },1000);
+
+                    },1000);
+
+                },1000);
+
+            },1000);
+
+        },1000);
+
+    });
+}
+
+callbackHell();
