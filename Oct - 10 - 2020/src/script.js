b302285caf9e0c3665a8d09e97ecd7b535ffe184
dheeraj <gumudavellidheeraj@gmail.com>
2020-10-10 22:01:28 +0530

    Jumbotron

--- Oct - 10 - 2020/src/script.js
+++ Oct - 10 - 2020/src/script.js
@@ -1,3 +1,9 @@
 function submitForm(){
     alert("Thanks Your data is saved.!")
 }
+
+let $homeBtn = document.querySelector("homebtn");
+
+$homeBtn.addEventListener("click",function(){
+    window.location.href = 'index.html';
+})
